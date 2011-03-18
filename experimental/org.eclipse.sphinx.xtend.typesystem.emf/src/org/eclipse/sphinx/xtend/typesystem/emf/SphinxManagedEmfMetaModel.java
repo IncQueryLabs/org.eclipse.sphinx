77fabd2081f65adf0ee85ce3877f7679f954080a
seberle <seberle@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2011-03-18 16:55:58 +0000

    Initial import

---
+++ experimental/org.eclipse.sphinx.xtend.typesystem.emf/src/org/eclipse/sphinx/xtend/typesystem/emf/SphinxManagedEmfMetaModel.java
@@ -0,0 +1,58 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2011 See4sys and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *     See4sys - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.eclipse.sphinx.xtend.typesystem.emf;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.sphinx.emf.model.IModelDescriptor;
+import org.eclipse.sphinx.emf.model.ModelDescriptorRegistry;
+import org.eclipse.xtend.typesystem.emf.EmfRegistryMetaModel;
+
+public class SphinxManagedEmfMetaModel extends EmfRegistryMetaModel {
+
+	protected IProject contextProject = null;
+	protected IModelDescriptor contextModel = null;
+
+	public SphinxManagedEmfMetaModel(IProject contextProject) {
+		this.contextProject = contextProject;
+	}
+
+	public SphinxManagedEmfMetaModel(IModelDescriptor contextModel) {
+		this.contextModel = contextModel;
+	}
+
+	private IModelDescriptor getModelsInContext() {
+	}
+
+	@Override
+	protected EPackage[] allPackages() {
+
+		final Collection<EPackage> ePackages = getAllEPackages(project.getProject());
+		return ePackages.toArray(new EPackage[ePackages.size()]);
+	}
+
+	protected Collection<EPackage> getAllEPackages(IProject project) {
+		Set<EPackage> allEPackages = new HashSet<EPackage>();
+		for (IModelDescriptor modelDescriptor : ModelDescriptorRegistry.INSTANCE.getModels(project)) {
+			allEPackages.addAll(modelDescriptor.getMetaModelDescriptor().getEPackages());
+		}
+		return Collections.unmodifiableSet(allEPackages);
+	}
+}
