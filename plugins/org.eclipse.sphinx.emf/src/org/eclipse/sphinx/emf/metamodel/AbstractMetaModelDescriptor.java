cd23a7b67e695adb5c0a5cb3d374e1edae23fef8
seberle <seberle@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2011-03-25 16:29:36 +0000

    RESOLVED - bug 340550: Add method returning a list of all metamodel EPackages to IMetaModelDescriptor https://bugs.eclipse.org/bugs/show_bug.cgi?id=340550

--- plugins/org.eclipse.sphinx.emf/src/org/eclipse/sphinx/emf/metamodel/AbstractMetaModelDescriptor.java
+++ plugins/org.eclipse.sphinx.emf/src/org/eclipse/sphinx/emf/metamodel/AbstractMetaModelDescriptor.java
@@ -268,8 +268,8 @@ public abstract class AbstractMetaModelDescriptor extends PlatformObject impleme
 				fRootEPackage = getEPackageRegistry().getEPackage(getNamespace());
 				if (fRootEPackage == null) {
 					// FIXME Consider to remove this part. In case that an older metamodel version has had a root
-					// EPackage
-					// but the latest metamodel version doesn't proceeding this way cannot be possibly a good thing
+					// EPackage but the latest metamodel version doesn't proceeding this way cannot be possibly a good
+					// thing
 					// Refer to compatible namespaces URI if no package can be found under native namespace URI
 					for (URI compatibleNamespaceURI : getCompatibleNamespaceURIs()) {
 						fRootEPackage = getEPackageRegistry().getEPackage(compatibleNamespaceURI.toString());
