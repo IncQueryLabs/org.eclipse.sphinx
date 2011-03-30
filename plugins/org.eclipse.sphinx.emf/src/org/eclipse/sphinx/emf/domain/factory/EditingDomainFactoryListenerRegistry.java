b08ee722139ae6de090362dbe83cec05221cc04b
seberle <seberle@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2011-03-30 21:24:02 +0000

    RESOLVED - bug 340010: EditingDomainFactoryListenerRegistry shall not create executable extension on start up https://bugs.eclipse.org/bugs/show_bug.cgi?id=340010

--- plugins/org.eclipse.sphinx.emf/src/org/eclipse/sphinx/emf/domain/factory/EditingDomainFactoryListenerRegistry.java
+++ plugins/org.eclipse.sphinx.emf/src/org/eclipse/sphinx/emf/domain/factory/EditingDomainFactoryListenerRegistry.java
@@ -23,10 +23,13 @@ import java.util.Set;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.osgi.util.NLS;
 import org.eclipse.sphinx.emf.Activator;
+import org.eclipse.sphinx.emf.internal.messages.Messages;
 import org.eclipse.sphinx.emf.metamodel.IMetaModelDescriptor;
 import org.eclipse.sphinx.emf.metamodel.MetaModelDescriptorRegistry;
 import org.eclipse.sphinx.platform.util.PlatformLogUtil;
+import org.osgi.framework.Bundle;
 
 /**
  * The singleton instance that is responsible for managing {@linkplain ITransactionalEditingDomainFactoryListener
@@ -59,15 +62,49 @@ public class EditingDomainFactoryListenerRegistry {
 	private static final String ATTR_MMDESC_ID_PATTERN = "metaModelDescriptorIdPattern"; //$NON-NLS-1$
 
 	/**
-	 * The registered {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listener}s.
+	 * The registered {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listener}s or
+	 * {@linkplain ListenerDescriptor listener descriptor}s
 	 */
-	private Map<IMetaModelDescriptor, Map<String, ITransactionalEditingDomainFactoryListener>> fEditingDomainFactoryListeners = new HashMap<IMetaModelDescriptor, Map<String, ITransactionalEditingDomainFactoryListener>>();
+	private Map<IMetaModelDescriptor, Map<String, Object>> fEditingDomainFactoryListeners = new HashMap<IMetaModelDescriptor, Map<String, Object>>();
 
 	/**
 	 * The {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listener}s which are overridden
 	 * by other ones.
 	 */
 	private Map<String, Set<IMetaModelDescriptor>> fOutstandingOverrides = new HashMap<String, Set<IMetaModelDescriptor>>();
+
+	/**
+	 * The descriptor that is used to store the listener information prior to instantiation
+	 */
+	private class ListenerDescriptor {
+
+		private String className;
+		private String contributorPluginId;
+
+		/**
+		 * @param className
+		 * @param contributorPluginId
+		 */
+		private ListenerDescriptor(String className, String contributorPluginId) {
+			this.className = className;
+			this.contributorPluginId = contributorPluginId;
+		}
+
+		/**
+		 * Construct and return the listener. This method might trigger plugin loading.
+		 * 
+		 * @return the listener
+		 * @throws ClassNotFoundException
+		 * @throws IllegalAccessException
+		 * @throws InstantiationException
+		 */
+		private ITransactionalEditingDomainFactoryListener getListener() throws ClassNotFoundException, InstantiationException,
+				IllegalAccessException {
+			Bundle bundle = Platform.getBundle(contributorPluginId);
+			Class<?> factoryClz = bundle.loadClass(className);
+			return (ITransactionalEditingDomainFactoryListener) factoryClz.newInstance();
+		}
+	}
 
 	/**
 	 * Private constructor for singleton pattern.
@@ -86,14 +123,13 @@ public class EditingDomainFactoryListenerRegistry {
 					String listenerId = cfgElement.getAttribute(ATTR_ID);
 					String overriddenListenerId = cfgElement.getAttribute(ATTR_OVERRIDE);
 
-					// Get instance of contributed editing domain factory listener
-					ITransactionalEditingDomainFactoryListener listener = (ITransactionalEditingDomainFactoryListener) cfgElement
-							.createExecutableExtension(ATTR_CLASS);
+					// Create a descriptor for the listener
+					ListenerDescriptor descriptor = new ListenerDescriptor(cfgElement.getAttribute(ATTR_CLASS), cfgElement.getContributor().getName());
 
 					// Register it upon meta-model descriptor is must be available for
 					for (IConfigurationElement applicableFor : cfgElement.getChildren(NODE_APPLICABLE_FOR)) {
 						String mmDescIdPattern = applicableFor.getAttribute(ATTR_MMDESC_ID_PATTERN);
-						addListener(mmDescIdPattern, listenerId, listener, overriddenListenerId);
+						addListenerDescriptor(mmDescIdPattern, listenerId, descriptor, overriddenListenerId);
 					}
 				}
 			} catch (Exception ex) {
@@ -104,13 +140,93 @@ public class EditingDomainFactoryListenerRegistry {
 		// Remove overridden editing domain factory listeners that couldn't be removed earlier
 		for (String overriddenListenerId : fOutstandingOverrides.keySet()) {
 			for (IMetaModelDescriptor mmDescriptor : fOutstandingOverrides.get(overriddenListenerId)) {
-				Map<String, ITransactionalEditingDomainFactoryListener> listenersForMetaModel = fEditingDomainFactoryListeners.get(mmDescriptor);
+				Map<String, Object> listenersForMetaModel = fEditingDomainFactoryListeners.get(mmDescriptor);
 				if (listenersForMetaModel != null) {
 					listenersForMetaModel.remove(overriddenListenerId);
 				}
 			}
 		}
 		fOutstandingOverrides.clear();
+	}
+
+	/**
+	 * Adds the specified {@linkplain ListenerDescriptor listener descriptor} to the map of registered listeners and
+	 * descriptors that have been contributed to the platform through
+	 * <tt>org.eclipse.sphinx.emf.editingDomainFactoryListeners</tt> extension point.
+	 * 
+	 * @param mmDescIdPattern
+	 *            The {@linkplain IMetaModelDescriptor meta-model descriptor} identifier pattern allowing to retrieve
+	 *            the meta-model descriptors the specified {@linkplain ITransactionalEditingDomainFactoryListener
+	 *            factory listener} must be associated to. Should not be <code>null</code>.
+	 * @param listenerId
+	 *            The identifier of the {@linkplain ITransactionalEditingDomainFactoryListener transactional editing
+	 *            domain factory listener} to be registered. Should not be null.
+	 * @param descriptor
+	 *            The {@linkplain ListenerDescriptor listener descriptor} that must be registered on the
+	 *            {@linkplain EditingDomainFactoryListenerRegistry factory listener registry} for the
+	 *            {@linkplain IMetaModelDescriptor meta-model descriptor}s that match the specified
+	 *            <code>mmDescIdPattern</code>. Should not be <code>null</code>.
+	 * @param overriddenListenerId
+	 *            The identifier of some other {@linkplain ITransactionalEditingDomainFactoryListener transactional
+	 *            editing domain factory listener} to be overridden or <code>null</code> when no overriding is required.
+	 */
+	private void addListenerDescriptor(String mmDescIdPattern, String listenerId, ListenerDescriptor descriptor, String overriddenListenerId) {
+		if (".*".equals(mmDescIdPattern) || ".+".equals(mmDescIdPattern)) { //$NON-NLS-1$ //$NON-NLS-2$
+			addListenerDescriptor(MetaModelDescriptorRegistry.ANY_MM, listenerId, descriptor, overriddenListenerId);
+			return;
+		}
+
+		for (IMetaModelDescriptor mmDescriptor : MetaModelDescriptorRegistry.INSTANCE.getDescriptors(mmDescIdPattern)) {
+			addListenerDescriptor(mmDescriptor, listenerId, descriptor, overriddenListenerId);
+		}
+	}
+
+	/**
+	 * Adds the specified {@linkplain ListenerDescriptor listener descriptor} to the map of registered listeners and
+	 * descriptors that have been contributed to the platform through
+	 * <tt>org.eclipse.sphinx.emf.editingDomainFactoryListeners</tt> extension point.
+	 * 
+	 * @param mmDescriptor
+	 *            The {@linkplain IMetaModelDescriptor meta-model descriptor} the specified
+	 *            {@linkplain ITransactionalEditingDomainFactoryListener factory listener} must be associated to. Should
+	 *            not be <code>null</code>.
+	 * @param listenerId
+	 *            The identifier of the {@linkplain ITransactionalEditingDomainFactoryListener transactional editing
+	 *            domain factory listener} to be registered. Shall not be null.
+	 * @param descriptor
+	 *            The {@linkplain ListenerDescriptor listener descriptor} that must be registered on the
+	 *            {@linkplain EditingDomainFactoryListenerRegistry factory listener registry} for the
+	 *            {@linkplain IMetaModelDescriptor meta-model descriptor}s that match the specified
+	 *            <code>mmDescIdPattern</code>. Should not be <code>null</code>.
+	 * @param overriddenListenerId
+	 *            The identifier of some other {@linkplain ITransactionalEditingDomainFactoryListener transactional
+	 *            editing domain factory listener} to be overridden or <code>null</code> when no overriding is required.
+	 */
+	private void addListenerDescriptor(IMetaModelDescriptor mmDescriptor, String listenerId, ListenerDescriptor descriptor,
+			String overriddenListenerId) {
+		if (mmDescriptor != null && descriptor != null) {
+			// Register editing domain factory listener for given meta-model descriptor
+			Map<String, Object> listenersForMetaModel = fEditingDomainFactoryListeners.get(mmDescriptor);
+			if (listenersForMetaModel == null) {
+				listenersForMetaModel = new HashMap<String, Object>();
+				fEditingDomainFactoryListeners.put(mmDescriptor, listenersForMetaModel);
+			}
+			listenersForMetaModel.put(listenerId, descriptor);
+
+			// Manage overridden editing domain factory listener, if any
+			if (overriddenListenerId != null) {
+				// Remove overridden editing domain factory listener; remember it for being removed later if it is not
+				// present yet
+				if (listenersForMetaModel.remove(overriddenListenerId) == null) {
+					Set<IMetaModelDescriptor> overridesForListener = fOutstandingOverrides.get(overriddenListenerId);
+					if (overridesForListener == null) {
+						overridesForListener = new HashSet<IMetaModelDescriptor>();
+						fOutstandingOverrides.put(overriddenListenerId, overridesForListener);
+					}
+					overridesForListener.add(mmDescriptor);
+				}
+			}
+		}
 	}
 
 	/**
@@ -125,28 +241,79 @@ public class EditingDomainFactoryListenerRegistry {
 	public Collection<ITransactionalEditingDomainFactoryListener> getListeners(IMetaModelDescriptor mmDescriptor) {
 		// Retrieve editing domain factory listeners registered upon specified meta-model descriptor or one of its super
 		// descriptors
-		HashSet<ITransactionalEditingDomainFactoryListener> listeners = new HashSet<ITransactionalEditingDomainFactoryListener>();
+		Set<ITransactionalEditingDomainFactoryListener> listeners = new HashSet<ITransactionalEditingDomainFactoryListener>();
 		for (IMetaModelDescriptor registeredMMDescriptor : fEditingDomainFactoryListeners.keySet()) {
 			if (registeredMMDescriptor.getClass().isInstance(mmDescriptor)) {
-				Map<String, ITransactionalEditingDomainFactoryListener> listenersForMetaModel = fEditingDomainFactoryListeners
-						.get(registeredMMDescriptor);
-				if (listenersForMetaModel != null) {
-					listeners.addAll(listenersForMetaModel.values());
-				}
+				Map<String, Object> listenersForMetaModel = fEditingDomainFactoryListeners.get(registeredMMDescriptor);
+				listeners.addAll(getListeners(listenersForMetaModel));
 			}
 		}
 
 		// Retrieve editing domain factory listeners registered upon static any meta-model descriptor
-		Map<String, ITransactionalEditingDomainFactoryListener> listenersForAnyMetaModel = fEditingDomainFactoryListeners
-				.get(MetaModelDescriptorRegistry.ANY_MM);
+		Map<String, Object> listenersForAnyMetaModel = fEditingDomainFactoryListeners.get(MetaModelDescriptorRegistry.ANY_MM);
 		if (listenersForAnyMetaModel != null) {
-			listeners.addAll(listenersForAnyMetaModel.values());
+			listeners.addAll(getListeners(listenersForAnyMetaModel));
 		}
 		return listeners;
 	}
 
 	/**
-	 * Adds the specified {@linkplain ITransactionalEditingDomainFactoryListener factory listener} into the map of
+	 * Returns a set of {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listeners} that
+	 * are contained or described by the entries in given <code>listenersOrDescriptors</code> map.
+	 * <p>
+	 * Depending on the type of the listener object contained by each map entry one of the following actions is taken:
+	 * <ul>
+	 * <li>Instances of {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listener}s are
+	 * directly added to the resulting listener set.</li>
+	 * <li>Instances of {@linkplain ListenerDescriptor editing domain factory listener descriptor}s are used to create
+	 * the actual {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listener}s and add those
+	 * to the resulting listener set. In addition the formers are replaced by the latters in given
+	 * <code>listenersOrDescriptors</code> map.</li>
+	 * <li>Instances of anything else are logged as errors and removed from given <code>listenersOrDescriptors</code>
+	 * map.
+	 * </p>
+	 * 
+	 * @param listenersOrDescriptors
+	 *            The map with {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listener}s
+	 *            or {@linkplain ListenerDescriptor editing domain factory listener descriptor}s and their respective
+	 *            identifiers to be processed.
+	 * @return A set of {@linkplain ITransactionalEditingDomainFactoryListener editing domain factory listener}s that
+	 *         are contained or described by the entries in given <code>listenersOrDescriptors </code> map.
+	 */
+	private Set<ITransactionalEditingDomainFactoryListener> getListeners(Map<String, Object> listenersOrDescriptors) {
+		Set<ITransactionalEditingDomainFactoryListener> listeners = new HashSet<ITransactionalEditingDomainFactoryListener>();
+		for (Iterator<String> iter = listenersOrDescriptors.keySet().iterator(); iter.hasNext();) {
+			String listenerId = iter.next();
+			Object listenerObject = listenersOrDescriptors.get(listenerId);
+			if (listenerObject instanceof ITransactionalEditingDomainFactoryListener) {
+				// Add existing listener
+				listeners.add((ITransactionalEditingDomainFactoryListener) listenerObject);
+			} else if (listenerObject instanceof ListenerDescriptor) {
+				// Create the listener
+				try {
+					ITransactionalEditingDomainFactoryListener listener = ((ListenerDescriptor) listenerObject).getListener();
+					listenersOrDescriptors.put(listenerId, listener);
+					listeners.add(listener);
+				} catch (Exception ex) {
+					PlatformLogUtil.logAsError(Activator.getPlugin(), ex);
+
+					// Discard bad descriptor so as to avoid repeated runs into same problem
+					iter.remove();
+				}
+			} else {
+				// Should not happen...
+				PlatformLogUtil.logAsError(Activator.getPlugin(),
+						NLS.bind(Messages.error_invalidEditingDomainFactoryListenerObject, listenerObject.getClass().getName()));
+
+				// Discard bad descriptor so as to avoid repeated runs into same problem
+				iter.remove();
+			}
+		}
+		return listeners;
+	}
+
+	/**
+	 * Adds the specified {@linkplain ITransactionalEditingDomainFactoryListener factory listener} to the map of
 	 * registered listeners that have been contributed to the platform through
 	 * <tt>org.eclipse.sphinx.emf.editingDomainFactoryListeners</tt> extension point.
 	 * 
@@ -189,7 +356,7 @@ public class EditingDomainFactoryListenerRegistry {
 	}
 
 	/**
-	 * Adds the specified {@linkplain ITransactionalEditingDomainFactoryListener factory listener} into the map of
+	 * Adds the specified {@linkplain ITransactionalEditingDomainFactoryListener factory listener} to the map of
 	 * registered listeners that have been contributed to the platform through
 	 * <tt>org.eclipse.sphinx.emf.editingDomainFactoryListeners</tt> extension point.
 	 * 
@@ -216,9 +383,9 @@ public class EditingDomainFactoryListenerRegistry {
 			String overriddenListenerId) {
 		if (mmDescriptor != null && listener != null) {
 			// Register editing domain factory listener for given meta-model descriptor
-			Map<String, ITransactionalEditingDomainFactoryListener> listenersForMetaModel = fEditingDomainFactoryListeners.get(mmDescriptor);
+			Map<String, Object> listenersForMetaModel = fEditingDomainFactoryListeners.get(mmDescriptor);
 			if (listenersForMetaModel == null) {
-				listenersForMetaModel = new HashMap<String, ITransactionalEditingDomainFactoryListener>();
+				listenersForMetaModel = new HashMap<String, Object>();
 				fEditingDomainFactoryListeners.put(mmDescriptor, listenersForMetaModel);
 			}
 			listenersForMetaModel.put(listenerId != null ? listenerId : listener.toString(), listener);
@@ -250,12 +417,12 @@ public class EditingDomainFactoryListenerRegistry {
 	// FIXME Should we think about a solution allowing ONLY the removal of dynamically registered listeners?
 	public void removeListener(ITransactionalEditingDomainFactoryListener listener) {
 		if (listener != null) {
-			Iterator<Map<String, ITransactionalEditingDomainFactoryListener>> iter1;
+			Iterator<Map<String, Object>> iter1;
 			for (iter1 = fEditingDomainFactoryListeners.values().iterator(); iter1.hasNext();) {
-				Map<String, ITransactionalEditingDomainFactoryListener> listenersForMetaModel = iter1.next();
-				Iterator<ITransactionalEditingDomainFactoryListener> iter2;
+				Map<String, Object> listenersForMetaModel = iter1.next();
+				Iterator<Object> iter2;
 				for (iter2 = listenersForMetaModel.values().iterator(); iter2.hasNext();) {
-					ITransactionalEditingDomainFactoryListener listenerForMetaModel = iter2.next();
+					Object listenerForMetaModel = iter2.next();
 					if (listener.equals(listenerForMetaModel)) {
 						iter2.remove();
 					}
