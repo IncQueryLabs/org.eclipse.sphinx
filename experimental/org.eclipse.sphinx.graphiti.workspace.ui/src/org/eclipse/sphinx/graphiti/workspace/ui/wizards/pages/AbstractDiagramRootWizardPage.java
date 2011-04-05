5d935c53a36e3c98b9191a5dd47353314835fb2c
aakar <aakar@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2011-04-05 13:27:19 +0000

    Added copy right header.

--- experimental/org.eclipse.sphinx.graphiti.workspace.ui/src/org/eclipse/sphinx/graphiti/workspace/ui/wizards/pages/AbstractDiagramRootWizardPage.java
+++ experimental/org.eclipse.sphinx.graphiti.workspace.ui/src/org/eclipse/sphinx/graphiti/workspace/ui/wizards/pages/AbstractDiagramRootWizardPage.java
@@ -12,6 +12,20 @@
  * 
  * </copyright>
  */
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2008-2011 See4sys and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *     See4sys - Initial API and implementation
+ * 
+ * </copyright>
+ */
 package org.eclipse.sphinx.graphiti.workspace.ui.wizards.pages;
 
 import java.util.Vector;
