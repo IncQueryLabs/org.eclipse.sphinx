0a8d500b0a9fc3d01b1dbfa8c3a57d0ae542fb9f
seberle <seberle@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2011-09-26 10:45:16 +0000

    Added command, menu and handler for converting projects to Xtend/Xpand-enabled plug-in projects

---
+++ plugins/org.eclipse.sphinx.xtendxpand.ui/src/org/eclipse/sphinx/xtendxpand/ui/commands/handlers/ConvertToXtendXpandEnabledPluginProjectHandler.java
@@ -0,0 +1,46 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2011 itemis and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *     itemis - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.eclipse.sphinx.xtendxpand.ui.commands.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.sphinx.platform.ui.util.UIUtil;
+import org.eclipse.sphinx.xtend.typesystem.emf.ui.SphinxManagedEmfMetamodelContributor;
+import org.eclipse.sphinx.xtendxpand.ui.internal.messages.Messages;
+import org.eclipse.sphinx.xtendxpand.ui.jobs.ConvertToXtendXpandEnabledPluginProjectJob;
+
+/**
+ * Command hander that supports conversion of {@link IProject project}s to an Xtend/Xpand-enabled plug-in project.
+ */
+public class ConvertToXtendXpandEnabledPluginProjectHandler extends AbstractHandler {
+
+	/*
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	// TODO Support simultaneous conversion of multiple projects
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IProject project = UIUtil.unwrap(event, IProject.class);
+		if (project != null) {
+			ConvertToXtendXpandEnabledPluginProjectJob job = new ConvertToXtendXpandEnabledPluginProjectJob(
+					Messages.job_convertingToXtendXpandEnabledPluginProject, project);
+			job.getEnabledMetamodelContributorTypeNames().add(SphinxManagedEmfMetamodelContributor.class.getName());
+			job.schedule();
+		}
+
+		return null;
+	}
+}
