434c7cce32bb10994f6d6248e318d6b428275322
cknuechel <cknuechel@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2012-02-22 18:11:43 +0000

    Restored non-generated code.

--- examples/org.eclipse.sphinx.examples.hummingbird20/src/org/eclipse/sphinx/examples/hummingbird20/util/Hummingbird20ResourceFactoryImpl.java
+++ examples/org.eclipse.sphinx.examples.hummingbird20/src/org/eclipse/sphinx/examples/hummingbird20/util/Hummingbird20ResourceFactoryImpl.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2008-2011 See4sys and others.
+ * Copyright (c) 2008-2010 See4sys and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,43 +14,110 @@
  */
 package org.eclipse.sphinx.examples.hummingbird20.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.xerces.impl.Constants;
 import org.eclipse.emf.common.util.URI;
-
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EPackageRegistryImpl;
 import org.eclipse.emf.ecore.resource.Resource;
-
 import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.sphinx.emf.resource.BasicMigrationExtendedMetaData;
+import org.eclipse.sphinx.emf.resource.ExtendedResource;
+import org.eclipse.sphinx.emf.resource.SchemaLocationURIHandler;
+import org.eclipse.sphinx.examples.hummingbird.ide.metamodel.HummingbirdMMDescriptor;
+import org.eclipse.sphinx.examples.hummingbird20.Activator;
+import org.eclipse.sphinx.examples.hummingbird20.common.Common20Package;
+import org.eclipse.sphinx.examples.hummingbird20.instancemodel.InstanceModel20Package;
+import org.eclipse.sphinx.examples.hummingbird20.typemodel.TypeModel20Package;
 
 /**
- * <!-- begin-user-doc -->
  * The <b>Resource Factory</b> associated with the package.
- * <!-- end-user-doc -->
+ * 
  * @see org.eclipse.sphinx.examples.hummingbird20.util.Hummingbird20ResourceImpl
- * @generated
+ * @generated NOT
  */
-public class Hummingbird20ResourceFactoryImpl extends ResourceFactoryImpl
-{
+public class Hummingbird20ResourceFactoryImpl extends ResourceFactoryImpl {
+
+	protected ExtendedMetaData extendedMetaData;
+
+	protected Map<String, String> schemaLocationCatalog;
+
+	protected SchemaLocationURIHandler schemaLocationURIHandler;
+
 	/**
 	 * Creates an instance of the resource factory.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * 
+	 * @generated NOT
 	 */
-	public Hummingbird20ResourceFactoryImpl()
-	{
-		super();
+	public Hummingbird20ResourceFactoryImpl() {
+		// Create and initialize schema location catalog, i.e., a map providing namespace and system identifier pairs
+		// that are allowed to be written in the resource's xsi:schemaLocation/xsi:noNamespaceSchemaLocation during
+		// saving
+		schemaLocationCatalog = new HashMap<String, String>();
+
+		// Register schema files for current metamodel version
+		schemaLocationCatalog.put(Common20Package.eNS_URI, "Common20XMI.xsd"); //$NON-NLS-1$
+		schemaLocationCatalog.put(TypeModel20Package.eNS_URI, "TypeModel20XMI.xsd"); //$NON-NLS-1$
+		schemaLocationCatalog.put(InstanceModel20Package.eNS_URI, "InstanceModel20XMI.xsd"); //$NON-NLS-1$
+		schemaLocationCatalog.put(XMIResource.XMI_URI, "XMI.xsd"); //$NON-NLS-1$
+
+		// Register schema files for compatible metamodel versions
+		schemaLocationCatalog.put(HummingbirdMMDescriptor.BASE_NAMESPACE + "/2.0.0/common", "Common200XMI.xsd"); //$NON-NLS-1$ //$NON-NLS-2$
+		schemaLocationCatalog.put(HummingbirdMMDescriptor.BASE_NAMESPACE + "/2.0.0/typemodel", "TypeModel200XMI.xsd"); //$NON-NLS-1$ //$NON-NLS-2$
+		schemaLocationCatalog.put(HummingbirdMMDescriptor.BASE_NAMESPACE + "/2.0.0/instancemodel", "InstanceModel200XMI.xsd"); //$NON-NLS-1$ //$NON-NLS-2$
+
+		// Create and initialize schema location URI handler enabling schema locations to be resolved to schema files
+		// during loading and schema files to be deresolved to schema locations during saving
+		schemaLocationURIHandler = new SchemaLocationURIHandler(schemaLocationCatalog);
+		schemaLocationURIHandler.addSchemaLocationBaseURI(Activator.getPlugin(), "model"); //$NON-NLS-1$
+		schemaLocationURIHandler.addSchemaLocationBaseURI(Activator.getPlugin(), "model/archive"); //$NON-NLS-1$ 
+
+		// Create and initialize migration-enabled extended meta data enabling Hummingbird resources whose version is
+		// not the same but compatible with that of current Hummingbird metamodel implementation to be loaded
+		extendedMetaData = new BasicMigrationExtendedMetaData(new EPackageRegistryImpl(EPackage.Registry.INSTANCE));
+
+		// Map relevant EPackages to their namespaces in current metamodel version
+		extendedMetaData.putPackage(TypeModel20Package.eNS_URI, TypeModel20Package.eINSTANCE);
+		extendedMetaData.putPackage(InstanceModel20Package.eNS_URI, InstanceModel20Package.eINSTANCE);
+
+		// Map relevant EPackages to their namespaces in compatible metamodel versions
+		extendedMetaData.putPackage(HummingbirdMMDescriptor.BASE_NAMESPACE + "/2.0.0/typemodel", TypeModel20Package.eINSTANCE); //$NON-NLS-1$
+		extendedMetaData.putPackage(HummingbirdMMDescriptor.BASE_NAMESPACE + "/2.0.0/instancemodel", InstanceModel20Package.eINSTANCE); //$NON-NLS-1$
 	}
 
 	/**
 	 * Creates an instance of the resource.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
+	 * 
+	 * @generated NOT
 	 */
 	@Override
-	public Resource createResource(URI uri)
-	{
-		Resource result = new Hummingbird20ResourceImpl(uri);
+	public Resource createResource(URI uri) {
+		XMIResource result = new Hummingbird20ResourceImpl(uri);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_LEXICAL_HANDLER, Boolean.TRUE);
+
+		// Configure SAX parser to continue even in case of fatal errors (typically XML well-formedness problems or I/O
+		// errors) so as to always load XML documents as far as possible rather than not loading the entire document
+		// just because a potentially small part of it is not good
+		Map<String, Boolean> parserFeatures = new HashMap<String, Boolean>();
+		parserFeatures.put(Constants.XERCES_FEATURE_PREFIX + Constants.CONTINUE_AFTER_FATAL_ERROR_FEATURE, true);
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_PARSER_FEATURES, parserFeatures);
+
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+		result.getDefaultSaveOptions().put(ExtendedResource.OPTION_SCHEMA_LOCATION_CATALOG, schemaLocationCatalog);
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_URI_HANDLER, schemaLocationURIHandler);
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_URI_HANDLER, schemaLocationURIHandler);
+
+		result.getDefaultLoadOptions().put(ExtendedResource.OPTION_ENABLE_SCHEMA_VALIDATION, Boolean.TRUE);
+
 		return result;
 	}
-
-} //Hummingbird20ResourceFactoryImpl
+} // Hummingbird20ResourceFactoryImpl
--- examples/org.eclipse.sphinx.examples.hummingbird20/src/org/eclipse/sphinx/examples/hummingbird20/util/Hummingbird20ResourceImpl.java
+++ examples/org.eclipse.sphinx.examples.hummingbird20/src/org/eclipse/sphinx/examples/hummingbird20/util/Hummingbird20ResourceImpl.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2008-2011 See4sys and others.
+ * Copyright (c) 2008-2010 See4sys and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,29 +14,109 @@
  */
 package org.eclipse.sphinx.examples.hummingbird20.util;
 
+import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.common.util.URI;
-
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.XMLHelper;
+import org.eclipse.emf.ecore.xmi.XMLLoad;
+import org.eclipse.emf.ecore.xmi.XMLSave;
+import org.eclipse.emf.ecore.xmi.impl.XMIHelperImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.sphinx.emf.resource.ExtendedResource;
+import org.eclipse.sphinx.emf.resource.ExtendedResourceAdapter;
+import org.eclipse.sphinx.emf.resource.ExtendedResourceAdapterFactory;
+import org.eclipse.sphinx.emf.resource.ExtendedSAXXMIHandler;
+import org.eclipse.sphinx.emf.resource.ExtendedXMILoadImpl;
+import org.eclipse.sphinx.emf.resource.ExtendedXMISaveImpl;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
- * <!-- begin-user-doc -->
  * The <b>Resource </b> associated with the package.
- * <!-- end-user-doc -->
+ * 
  * @see org.eclipse.sphinx.examples.hummingbird20.util.Hummingbird20ResourceFactoryImpl
  * @generated
  */
-public class Hummingbird20ResourceImpl extends XMIResourceImpl
-{
+public class Hummingbird20ResourceImpl extends XMIResourceImpl {
+
+	/**
+	 * {@link Adapter} providing Hummingbird-specific implementation of {@link ExtendedResource extended resource}
+	 * services.
+	 * 
+	 * @see ExtendedResource
+	 */
+	protected ExtendedResourceAdapter extendedResource;
+
 	/**
 	 * Creates an instance of the resource.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param uri the URI of the new resource.
-	 * @generated
+	 * 
+	 * @param uri
+	 *            the URI of the new resource.
+	 * @generated NOT
 	 */
-	public Hummingbird20ResourceImpl(URI uri)
-	{
+	public Hummingbird20ResourceImpl(URI uri) {
 		super(uri);
+
+		// Install adapter providing Hummingbird-specific implementation of extended resource services
+		extendedResource = new ExtendedHummingbirdResourceAdapter();
+		eAdapters().add(extendedResource);
 	}
 
-} //Hummingbird20ResourceImpl
+	/*
+	 * @see org.eclipse.emf.ecore.resource.impl.ResourceImpl#unloaded(org.eclipse.emf.ecore.InternalEObject)
+	 */
+	@Override
+	protected void unloaded(InternalEObject internalEObject) {
+		// Delegate to implementation provided by extended resource for enabling memory-optimized unloading
+		extendedResource.unloaded(internalEObject);
+	}
+
+	/*
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl#createXMLLoad()
+	 */
+	@Override
+	protected XMLLoad createXMLLoad() {
+		// Use extended XMILoad implementation for XMI to include support for on-the-fly resource migration,
+		// resource-centric problem handling, enhanced entity resolution and ignorable whitespace suppression
+		return new ExtendedXMILoadImpl(createXMLHelper()) {
+			@Override
+			protected DefaultHandler makeDefaultHandler() {
+				return new ExtendedSAXXMIHandler(resource, helper, options);
+			}
+		};
+	}
+
+	/*
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl#createXMLSave()
+	 */
+	@Override
+	protected XMLSave createXMLSave() {
+		// Use extended XMISave implementation for XMI to include support for on-the-fly resource migration and enhanced
+		// schema location support
+		return new ExtendedXMISaveImpl(createXMLHelper());
+	}
+
+	/*
+	 * @see org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl#createXMLHelper()
+	 */
+	@Override
+	protected XMLHelper createXMLHelper() {
+		// Enable URIs used for serializing cross-document references be customized through extended resource service
+		return new XMIHelperImpl(this) {
+			/*
+			 * @see org.eclipse.emf.ecore.xmi.impl.XMLHelperImpl#getHREF(org.eclipse.emf.ecore.resource.Resource,
+			 * org.eclipse.emf.ecore.EObject)
+			 */
+			@Override
+			protected URI getHREF(Resource otherResource, EObject obj) {
+				ExtendedResource otherExtendedResource = ExtendedResourceAdapterFactory.INSTANCE.adapt(otherResource);
+				if (otherExtendedResource != null) {
+					return otherExtendedResource.getURI(obj);
+				} else {
+					return super.getHREF(otherResource, obj);
+				}
+			}
+		};
+	}
+} // Hummingbird20ResourceImpl
