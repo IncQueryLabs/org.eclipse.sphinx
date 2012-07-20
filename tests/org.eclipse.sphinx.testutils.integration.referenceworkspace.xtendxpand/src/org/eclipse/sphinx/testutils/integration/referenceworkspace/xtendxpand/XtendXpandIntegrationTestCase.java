5d7ee5684bea6de8621b84158f315ede316d5234
seberle <seberle@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2012-07-20 07:13:05 +0000

    ASSIGNED - bug 384418: [Releng] Create Maven/Tycho build for Sphinx https://bugs.eclipse.org/bugs/show_bug.cgi?id=384418
    Refactored and cleaned up integration test runtime

--- tests/org.eclipse.sphinx.testutils.integration.referenceworkspace.xtendxpand/src/org/eclipse/sphinx/testutils/integration/referenceworkspace/xtendxpand/XtendXpandIntegrationTestCase.java
+++ tests/org.eclipse.sphinx.testutils.integration.referenceworkspace.xtendxpand/src/org/eclipse/sphinx/testutils/integration/referenceworkspace/xtendxpand/XtendXpandIntegrationTestCase.java
@@ -14,17 +14,11 @@
  */
 package org.eclipse.sphinx.testutils.integration.referenceworkspace.xtendxpand;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.eclipse.core.runtime.Plugin;
-import org.eclipse.sphinx.emf.mwe.IXtendXpandConstants;
 import org.eclipse.sphinx.testutils.integration.AbstractIntegrationTestCase;
 import org.eclipse.sphinx.testutils.integration.referenceworkspace.xtendxpand.internal.Activator;
 
 public class XtendXpandIntegrationTestCase extends AbstractIntegrationTestCase<XtendXpandTestReferenceWorkspace> {
-
-	private Map<String, String> fileEncodingRules = null;
 
 	public XtendXpandIntegrationTestCase() {
 		super(XtendXpandTestReferenceWorkspace.class.getName());
@@ -44,16 +38,5 @@ public class XtendXpandIntegrationTestCase extends AbstractIntegrationTestCase<X
 	protected String[][] getProjectReferences() {
 		return new String[][] { { XtendXpandTestReferenceWorkspace.HB_TRANSFORM_XTEND_PROJECT_NAME },
 				{ XtendXpandTestReferenceWorkspace.HB_CODEGEN_XPAND_PROJECT_NAME } };
-	}
-
-	@Override
-	protected Map<String, String> getFileEncodingRules() {
-		if (fileEncodingRules == null) {
-			fileEncodingRules = new HashMap<String, String>();
-			fileEncodingRules.put(IXtendXpandConstants.CHECK_EXTENSION, "UTF-8"); //$NON-NLS-1$
-			fileEncodingRules.put(IXtendXpandConstants.EXTENSION_EXTENSION, "UTF-8"); //$NON-NLS-1$
-			fileEncodingRules.put(IXtendXpandConstants.TEMPLATE_EXTENSION, "UTF-8"); //$NON-NLS-1$
-		}
-		return fileEncodingRules;
 	}
 }
