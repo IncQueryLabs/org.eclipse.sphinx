37d1291277049d5a9570c08df90342af9e404938
seberle <seberle@7a89e036-c7eb-11df-95f5-ad4a195f9937>
2012-10-19 09:26:59 +0000

    Fixed wrong BUNDLE_NAME

--- plugins/org.eclipse.sphinx.gmf.runtime.ui/src/org/eclipse/sphinx/gmf/runtime/ui/internal/messages/Messages.java
+++ plugins/org.eclipse.sphinx.gmf.runtime.ui/src/org/eclipse/sphinx/gmf/runtime/ui/internal/messages/Messages.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2008-2010 See4sys and others.
+ * Copyright (c) 2008-2012 See4sys, itemis and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  * 
  * Contributors: 
  *     See4sys - Initial API and implementation
+ *     itemis - Fixed wrong BUNDLE_NAME
  * 
  * </copyright>
  */
@@ -18,7 +19,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 
-	private static final String BUNDLE_NAME = "org.eclipse.sphinx.gmf.internal.messages.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.sphinx.gmf.runtime.ui.internal.messages.messages"; //$NON-NLS-1$
 
 	static {
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
