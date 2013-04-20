c3ae44d007d0ad5fd19491e9e6dda25e12853cd7
seberle <stephaneberle9@gmail.com>
2013-04-20 15:27:44 +0200

    RESOLVED - bug 406062: [Workspace Management] Removal of the required project nature parameter in NewModelFileCreationPage constructor and CreateNewModelProjectJob constructor https://bugs.eclipse.org/bugs/show_bug.cgi?id=406062
    Renamed NewModelFileProperties to InitialModelProperties

--- plugins/org.eclipse.sphinx.emf.workspace.ui/src/org/eclipse/sphinx/emf/workspace/ui/wizards/pages/InitialModelProperties.java
+++ plugins/org.eclipse.sphinx.emf.workspace.ui/src/org/eclipse/sphinx/emf/workspace/ui/wizards/pages/InitialModelProperties.java
@@ -12,31 +12,30 @@
  *
  * </copyright>
  */
-package org.eclipse.sphinx.emf.workspace.ui.wizards;
+package org.eclipse.sphinx.emf.workspace.ui.wizards.pages;
 
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.sphinx.emf.metamodel.IMetaModelDescriptor;
-import org.eclipse.sphinx.emf.workspace.ui.wizards.pages.NewInitialModelCreationPage;
-import org.eclipse.sphinx.emf.workspace.ui.wizards.pages.NewModelFileCreationPage;
+import org.eclipse.sphinx.emf.workspace.ui.wizards.AbstractNewModelFileWizard;
 
 /**
  * The properties of the initial model for a new model file, i.e., the IMetaModelDescriptor and the EPackage and the
  * EClassifier of the root object selected by the user. This class is used to share the property values between the
- * wizard pages and wizards, e.g., {@link NewInitialModelCreationPage} sets the property values, and
+ * wizard pages and wizards, e.g., {@link InitialModelCreationPage} sets the property values, and
  * {@link NewModelFileCreationPage} as well as {@link AbstractNewModelFileWizard} use the selected property values to
  * create the initial model and the new model file.
  */
-public class NewModelFileProperties<T extends IMetaModelDescriptor> {
+public class InitialModelProperties<T extends IMetaModelDescriptor> {
 
 	private T mmDescriptor;
 	private EPackage rootObjectEPackage;
 	private EClassifier rootObjectEClassifier;
 
-	public NewModelFileProperties() {
+	public InitialModelProperties() {
 	}
 
-	public NewModelFileProperties(T mmDescriptor) {
+	public InitialModelProperties(T mmDescriptor) {
 		this.mmDescriptor = mmDescriptor;
 	}
 
