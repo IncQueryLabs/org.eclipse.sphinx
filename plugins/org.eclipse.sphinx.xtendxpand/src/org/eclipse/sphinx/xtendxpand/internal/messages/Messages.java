8ec09e62b759373ad85985382eff7358573bfca1
Idrissa DIENG <idydieng@gmail.com>
2014-10-13 10:34:41 +0200

    Resolved https://bugs.eclipse.org/bugs/show_bug.cgi?id=445125

--- plugins/org.eclipse.sphinx.xtendxpand/src/org/eclipse/sphinx/xtendxpand/internal/messages/Messages.java
+++ plugins/org.eclipse.sphinx.xtendxpand/src/org/eclipse/sphinx/xtendxpand/internal/messages/Messages.java
@@ -1,15 +1,15 @@
 /**
  * <copyright>
- * 
+ *
  * Copyright (c) 2011 See4sys and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
+ *
+ * Contributors:
  *     See4sys - Initial API and implementation
- * 
+ *
  * </copyright>
  */
 package org.eclipse.sphinx.xtendxpand.internal.messages;
@@ -24,7 +24,6 @@ public class Messages extends NLS {
 	public static String task_ConvertingToJavaProject;
 	public static String task_ConvertingToPluginProject;
 	public static String task_ConvertingToXtendXpandProject;
-	public static String error_JRECompliance_NotSupported;
 	public static String error_JRECompliance_NotCompatible;
 
 	static {
