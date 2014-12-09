9a29494bf5c4290020a1fcbc3e05edbe4078f19b
Stephan Eberle <stephaneberle9@gmail.com>
2014-12-09 11:42:20 +0100

    Fixed incorrect copyright header

--- plugins/org.eclipse.sphinx.emf.mwe.dynamic.headless/src/org/eclipse/sphinx/emf/mwe/dynamic/headless/IWorkflowRunnerCLIConstants.java
+++ plugins/org.eclipse.sphinx.emf.mwe.dynamic.headless/src/org/eclipse/sphinx/emf/mwe/dynamic/headless/IWorkflowRunnerCLIConstants.java
@@ -1,14 +1,14 @@
 /**
  * <copyright>
  *
- * Copyright (c) See4sys and others.
- * All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Artop Software License Based on AUTOSAR
- * Released Material (ASLR) which accompanies this distribution, and is
- * available at http://www.artop.org/aslr.html
+ * Copyright (c) 2014 itemis and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     See4sys - Initial API and implementation
+ *     itemis - Initial API and implementation
  *
  * </copyright>
  */
