2f654cad38b93e3275624cbb717d77358aab9477
Idrissa DIENG <idydieng@gmail.com>
2015-01-27 14:14:33 +0100

    RESOLVED Bug 458518 - Add org.eclipse.sphinx.emf.editors plug-in

--- plugins/org.eclipse.sphinx.emf.editors/src/org/eclipse/sphinx/emf/editors/IModelEditorInputChangeAnalyzer.java
+++ plugins/org.eclipse.sphinx.emf.editors/src/org/eclipse/sphinx/emf/editors/IModelEditorInputChangeAnalyzer.java
@@ -1,18 +1,18 @@
 /**
  * <copyright>
- * 
- * Copyright (c) 2012 itemis and others.
+ *
+ * Copyright (c) 2014 itemis and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: 
+ *
+ * Contributors:
  *     itemis - Initial API and implementation
- * 
+ *
  * </copyright>
  */
-package org.eclipse.sphinx.gmf.runtime.ui.internal.editor;
+package org.eclipse.sphinx.emf.editors;
 
 import java.util.Set;
 
