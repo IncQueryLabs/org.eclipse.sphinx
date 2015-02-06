4b30f01badbce65af9ba02dd6ab0dccf3a8a3e5a
aakar <ali.akar82@gmail.com>
2015-02-06 16:21:43 +0100

    Hamronized show in context of different views

--- plugins/org.eclipse.sphinx.emf.check.ui/src/org/eclipse/sphinx/emf/check/ui/CheckValidationView.java
+++ plugins/org.eclipse.sphinx.emf.check.ui/src/org/eclipse/sphinx/emf/check/ui/CheckValidationView.java
@@ -16,17 +16,28 @@
 
 package org.eclipse.sphinx.emf.check.ui;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EValidator;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.sphinx.emf.util.EcorePlatformUtil;
+import org.eclipse.sphinx.emf.util.WorkspaceEditingDomainUtil;
 import org.eclipse.ui.part.IShowInSource;
 import org.eclipse.ui.part.ShowInContext;
 import org.eclipse.ui.views.markers.MarkerSupportView;
+import org.eclipse.ui.views.properties.IPropertySheetPage;
+import org.eclipse.ui.views.properties.tabbed.ITabbedPropertySheetPageContributor;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
-public class CheckValidationView extends MarkerSupportView {
+public class CheckValidationView extends MarkerSupportView implements ITabbedPropertySheetPageContributor {
+
+	protected Set<IPropertySheetPage> propertySheetPages = new HashSet<IPropertySheetPage>();
 
 	public CheckValidationView() {
 		super(IValidationUIConstants.VALIDATION_CHECK_MARKER_GENERATOR);
@@ -43,14 +54,60 @@ public class CheckValidationView extends MarkerSupportView {
 				@Override
 				public ShowInContext getShowInContext() {
 					IMarker[] markers = getSelectedMarkers();
-					List<IResource> resources = new ArrayList<IResource>();
-					for (IMarker marker : markers) {
-						resources.add(marker.getResource());
-					}
-					return new ShowInContext(new StructuredSelection(getSelectedMarkers()), new StructuredSelection(resources));
+					return new ShowInContext(new StructuredSelection(retrieveModelObjects(markers)), new StructuredSelection(
+							retrieveResources(markers)));
 				}
 			};
 		}
+		if (IPropertySheetPage.class == adapter) {
+			return getPropertySheetPage();
+		}
 		return super.getAdapter(adapter);
+	}
+
+	/**
+	 * This creates a new property sheet page instance and manages it in the cache.
+	 */
+	protected IPropertySheetPage getPropertySheetPage() {
+		IPropertySheetPage propertySheetPage = new TabbedPropertySheetPage(this);
+		propertySheetPages.add(propertySheetPage);
+		return propertySheetPage;
+	}
+
+	private IResource[] retrieveResources(IMarker[] markers) {
+		Set<IResource> resources = new HashSet<IResource>();
+		for (IMarker marker : markers) {
+			resources.add(marker.getResource());
+		}
+		return resources.toArray(new IResource[resources.size()]);
+	}
+
+	private Object[] retrieveModelObjects(IMarker[] markers) {
+		// Retrieve model objects behind problem markers
+		Set<Object> objects = new HashSet<Object>();
+		for (IMarker marker : markers) {
+			try {
+				if (marker.isSubtypeOf(EValidator.MARKER)) {
+					TransactionalEditingDomain editingDomain = WorkspaceEditingDomainUtil.getEditingDomain(marker.getResource());
+					if (editingDomain != null) {
+						String uriAttribute = marker.getAttribute(EValidator.URI_ATTRIBUTE, null);
+						if (uriAttribute != null) {
+							EObject object = EcorePlatformUtil.getEObject(editingDomain, URI.createURI(uriAttribute, true));
+							if (object != null) {
+								objects.add(object);
+							}
+						}
+					}
+				}
+			} catch (Exception ex) {
+				// Ignore exception, just continue with next marker
+			}
+		}
+		return objects.toArray();
+	}
+
+	@Override
+	public String getContributorId() {
+		return getSite().getId();
 	}
 }
---
+++ plugins/org.eclipse.sphinx.emf.ui/src/org/eclipse/sphinx/emf/ui/handlers/ExtendedNewPropertySheetHandler.java
@@ -0,0 +1,44 @@
+package org.eclipse.sphinx.emf.ui.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.views.properties.NewPropertySheetHandler;
+import org.eclipse.ui.views.properties.PropertySheet;
+import org.eclipse.ui.views.properties.PropertyShowInContext;
+
+public class ExtendedNewPropertySheetHandler extends NewPropertySheetHandler {
+
+	/**
+	 * Whether new PVs are pinned when newly opened
+	 */
+	private static final boolean PIN_NEW_PROPERTY_VIEW = Boolean.valueOf(
+			System.getProperty("org.eclipse.ui.views.properties.pinNewPV", Boolean.FALSE.toString())).booleanValue(); //$NON-NLS-1$
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchPart activePart = HandlerUtil.getActivePartChecked(event);
+
+		PropertyShowInContext context = getShowInContext(event);
+		try {
+			PropertySheet sheet = findPropertySheet(event, context);
+			Object input = context.getInput();
+			if (input instanceof IStructuredSelection) {
+				context.setSelection((IStructuredSelection) input);
+			}
+			sheet.show(context);
+			if (activePart instanceof PropertySheet) {
+				PropertySheet parent = (PropertySheet) activePart;
+				parent.setPinned(true);
+			} else if (!sheet.isPinned()) {
+				sheet.setPinned(PIN_NEW_PROPERTY_VIEW);
+			}
+		} catch (PartInitException e) {
+			throw new ExecutionException("Part could not be initialized", e); //$NON-NLS-1$
+		}
+		return null;
+	}
+}
