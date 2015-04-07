e4abfc8866d1172b9c578dd645da8dc7e10ac668
Stephan Eberle <stephaneberle9@gmail.com>
2015-04-07 15:17:40 +0200

    Added missing copyright header

--- plugins/org.eclipse.sphinx.emf.check/src/org/eclipse/sphinx/emf/check/util/ExtendedEObjectValidator.java
+++ plugins/org.eclipse.sphinx.emf.check/src/org/eclipse/sphinx/emf/check/util/ExtendedEObjectValidator.java
@@ -1,3 +1,17 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2015 itemis and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     itemis - Initial API and implementation
+ *
+ * </copyright>
+ */
 package org.eclipse.sphinx.emf.check.util;
 
 import java.util.Map;
@@ -9,6 +23,7 @@ import org.eclipse.emf.ecore.util.EObjectValidator;
 
 public class ExtendedEObjectValidator extends EObjectValidator {
 
+	// Overridden to make this method public
 	@Override
 	public boolean validate(int classifierID, Object object, DiagnosticChain diagnostics, Map<Object, Object> context) {
 		return classifierID != EcorePackage.EOBJECT || validate_EveryDefaultConstraint((EObject) object, diagnostics, context);
