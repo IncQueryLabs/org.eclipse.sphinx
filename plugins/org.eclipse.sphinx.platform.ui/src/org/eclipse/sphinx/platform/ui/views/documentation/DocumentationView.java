a2691da1ecdb4c1e4efec61030827284728f05b8
Idrissa DIENG <idydieng@gmail.com>
2015-04-07 10:21:16 +0200

    In progress Bug 462050 - Contribution: DocumentationView

--- plugins/org.eclipse.sphinx.platform.ui/src/org/eclipse/sphinx/platform/ui/views/documentation/DocumentationView.java
+++ plugins/org.eclipse.sphinx.platform.ui/src/org/eclipse/sphinx/platform/ui/views/documentation/DocumentationView.java
@@ -17,14 +17,6 @@ package org.eclipse.sphinx.platform.ui.views.documentation;
 import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.core.expressions.EvaluationContext;
-import org.eclipse.core.expressions.EvaluationResult;
-import org.eclipse.core.expressions.Expression;
-import org.eclipse.core.expressions.ExpressionConverter;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.sphinx.platform.ui.internal.Activator;
@@ -41,11 +33,8 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 
 import com.google.common.base.Function;
-import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Ordering;
-import com.google.common.primitives.Ints;
 
 /**
  * The view to show any additional documentation. It collects the data to actually display through various extension
@@ -58,32 +47,8 @@ public class DocumentationView extends ViewPart {
 	 */
 	public static final String ID = "org.eclipse.sphinx.platform.ui.views.documentation.DocumentationView"; //$NON-NLS-1$
 
-	private static final String EXTENSION_POINT_DOCUMENTATION_VIEW_FORMATTERS = "org.eclipse.sphinx.platform.ui.documentationViewFormatters"; //$NON-NLS-1$
-	private static final String NODE_INSTANCEOF = "instanceof"; //$NON-NLS-1$
-	private static final String ATTR_CLASS = "class"; //$NON-NLS-1$
-	private static final String ATTR_PRIORITY = "priority"; //$NON-NLS-1$
-
 	private Browser browser;
 	protected IDescriptionWrapper wrapper = new BootstrapDescriptionWrapper();
-	private IExtensionRegistry extensionRegistry;
-
-	protected Ordering<IConfigurationElement> configOrdering = new Ordering<IConfigurationElement>() {
-
-		@Override
-		public int compare(IConfigurationElement left, IConfigurationElement right) {
-			String leftPriority = left.getAttribute(ATTR_PRIORITY);
-			String rightPriority = right.getAttribute(ATTR_PRIORITY);
-			int leftPriorityInt = 0;
-			int rightPriorityInt = 0;
-			if (leftPriority.length() > 0) {
-				leftPriorityInt = Integer.parseInt(leftPriority);
-			}
-			if (rightPriority.length() > 0) {
-				rightPriorityInt = Integer.parseInt(rightPriority);
-			}
-			return Ints.compare(leftPriorityInt, rightPriorityInt);
-		}
-	};
 
 	/*
 	 * The content provider class is responsible for providing objects to the view. It can wrap existing objects in
@@ -105,15 +70,18 @@ public class DocumentationView extends ViewPart {
 
 			while (iterator.hasNext()) {
 				final Object selectedObject = iterator.next();
-				List<IConfigurationElement> applicableDocViewFormatters = configOrdering.sortedCopy(getApplicableDocViewFormatterfor(selectedObject));
-				Iterable<IDocumentationViewFormatter> formatters = Iterables.transform(applicableDocViewFormatters,
-						new Function<IConfigurationElement, IDocumentationViewFormatter>() {
+				Iterable<DocumentationViewFormatterDescriptor> applicableDocViewFormatters = DocumentationViewFormatterDescriptorRegistry.INSTANCE
+						.getApplicableDocViewFormatterFor(selectedObject);
+				List<DocumentationViewFormatterDescriptor> sortedApplicableDocViewFormatters = DocumentationViewFormatterDescriptorRegistry.INSTANCE.docViewFormatterOrdering
+						.sortedCopy(applicableDocViewFormatters);
+				Iterable<IDocumentationViewFormatter> formatters = Iterables.transform(sortedApplicableDocViewFormatters,
+						new Function<DocumentationViewFormatterDescriptor, IDocumentationViewFormatter>() {
 
 							@Override
-							public IDocumentationViewFormatter apply(IConfigurationElement input) {
+							public IDocumentationViewFormatter apply(DocumentationViewFormatterDescriptor descriptor) {
 								try {
-									return (IDocumentationViewFormatter) input.createExecutableExtension(ATTR_CLASS);
-								} catch (CoreException ex) {
+									return descriptor.getInstance();
+								} catch (Exception ex) {
 									PlatformLogUtil.logAsError(Activator.getDefault(), ex);
 									return null;
 								}
@@ -143,41 +111,6 @@ public class DocumentationView extends ViewPart {
 			browser.setText(BootstrapFormatterHTML.format(topTitle, builder.toString(), allSections));
 		}
 	};
-
-	private IExtensionRegistry getExtensionRegistry() {
-		if (extensionRegistry == null) {
-			extensionRegistry = Platform.getExtensionRegistry();
-		}
-		return extensionRegistry;
-	}
-
-	protected IConfigurationElement[] readContributedDocViewFormatters() {
-		IExtensionRegistry extensionRegistry = getExtensionRegistry();
-		if (extensionRegistry != null) {
-			return extensionRegistry.getConfigurationElementsFor(EXTENSION_POINT_DOCUMENTATION_VIEW_FORMATTERS);
-		}
-		return new IConfigurationElement[0];
-	}
-
-	protected Iterable<IConfigurationElement> getApplicableDocViewFormatterfor(final Object selectedObject) {
-		Iterable<IConfigurationElement> applicableDocViewFormatters = Iterables.filter(Lists.newArrayList(readContributedDocViewFormatters()),
-				new Predicate<IConfigurationElement>() {
-
-					@Override
-					public boolean apply(IConfigurationElement configElement) {
-						try {
-							IConfigurationElement instanceOfConfigElement = configElement.getChildren(NODE_INSTANCEOF)[0];
-							Expression expression = ExpressionConverter.getDefault().perform(instanceOfConfigElement);
-							EvaluationResult evaluate = expression.evaluate(new EvaluationContext(null, selectedObject));
-							return evaluate.equals(EvaluationResult.TRUE);
-						} catch (Exception ex) {
-							PlatformLogUtil.logAsError(Activator.getDefault(), ex);
-							return false;
-						}
-					}
-				});
-		return applicableDocViewFormatters;
-	}
 
 	/**
 	 * This is a callback that will allow us to create the viewer and initialize it.
