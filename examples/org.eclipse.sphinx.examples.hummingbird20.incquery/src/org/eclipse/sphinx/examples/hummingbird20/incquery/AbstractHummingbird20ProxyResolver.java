971760febbd47a009fe914f7b9cb0c78db1ec3c3
seberle <stephaneberle9@gmail.com>
2015-08-27 12:28:02 +0200

    Replaced locally defined URI separator constants by those provided on ExtendedResource

--- examples/org.eclipse.sphinx.examples.hummingbird20.incquery/src/org/eclipse/sphinx/examples/hummingbird20/incquery/AbstractHummingbird20ProxyResolver.java
+++ examples/org.eclipse.sphinx.examples.hummingbird20.incquery/src/org/eclipse/sphinx/examples/hummingbird20/incquery/AbstractHummingbird20ProxyResolver.java
@@ -1,3 +1,18 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2014-2015 itemis and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     itemis - Initial API and implementation
+ *     itemis - 475954: Proxies with fragment-based proxy URIs may get resolved across model boundaries
+ *
+ * </copyright>
+ */
 package org.eclipse.sphinx.examples.hummingbird20.incquery;
 
 import org.eclipse.emf.common.util.URI;
@@ -6,14 +21,12 @@ import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.incquery.runtime.api.IncQueryEngine;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
 import org.eclipse.sphinx.emf.incquery.proxymanagment.AbstractProxyResolver;
+import org.eclipse.sphinx.emf.resource.ExtendedResource;
 import org.eclipse.sphinx.examples.hummingbird20.common.Identifiable;
 import org.eclipse.sphinx.examples.hummingbird20.incquery.internal.Activator;
 import org.eclipse.sphinx.platform.util.PlatformLogUtil;
 
 public abstract class AbstractHummingbird20ProxyResolver extends AbstractProxyResolver {
-
-	public static final String QUERY_SEPARATOR = "?"; //$NON-NLS-1$
-	public static final String SEGMENT_SEPARATOR = "/"; //$NON-NLS-1$
 
 	@Override
 	protected EObject[] getEObjectCandidates(EObject proxy, Object contextObject, IncQueryEngine engine) {
@@ -43,9 +56,9 @@ public abstract class AbstractHummingbird20ProxyResolver extends AbstractProxyRe
 			// Retrieve absolute qualified name of given ARObject from its proxy URI
 			String uriFragment = internalEObject.eProxyURI().fragment();
 			if (uriFragment != null) {
-				int queryStringIndex = uriFragment.indexOf(QUERY_SEPARATOR);
+				int queryStringIndex = uriFragment.indexOf(ExtendedResource.URI_QUERY_SEPARATOR);
 				String aqn = queryStringIndex != -1 ? uriFragment.substring(0, queryStringIndex) : uriFragment;
-				int segSeparatorLastIndex = aqn.lastIndexOf(SEGMENT_SEPARATOR);
+				int segSeparatorLastIndex = aqn.lastIndexOf(ExtendedResource.URI_SEGMENT_SEPARATOR);
 				return segSeparatorLastIndex != -1 ? aqn.substring(segSeparatorLastIndex + 1) : aqn;
 			}
 			return ""; //$NON-NLS-1$
